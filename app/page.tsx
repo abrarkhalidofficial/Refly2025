5385ae051c1b4aa55d2a2d38e4c99c51835baed3
M Abrar Khalid <abrarprince471@gmail.com>
2025-08-22 17:27:25 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import HeroSection from "@/components/home-section";
 import AboutSection from "@/components/about-section";
 import CardsSection from "@/components/cards-section";
+import DesignSection from "@/components/design-section";
+import BusinessSection from "@/components/business-section";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <CardsSection />
+      <DesignSection />
+      <BusinessSection />
     </>
   );
 }
