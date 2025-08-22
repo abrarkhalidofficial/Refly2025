e56caf176054b231f059851a655230641cb7bb8c
M Abrar Khalid <abrarprince471@gmail.com>
2025-08-22 12:26:11 +0500

    Update cards-section.tsx

--- components/cards-section.tsx
+++ components/cards-section.tsx
@@ -6,8 +6,8 @@ import React from "react";
 export default function Services() {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center gap-6 px-6 py-10">
-      <div className="flex flex-wrap w-full max-w-7xl">
-        <div className="relative rounded-2xl overflow-hidden group bg-neutral-600 p-6 flex flex-col justify-between w-[35%] mb-6 mr-4">
+      <div className="flex flex-wrap w-full min-h-screen p-8">
+        <div className="relative rounded-4xl  overflow-hidden group bg-neutral-500 p-6 flex flex-col justify-between w-[39%] mb-6 mr-4">
           <video
             src="/IMG_3421.mp4"
             autoPlay
@@ -18,23 +18,25 @@ export default function Services() {
           />
           <div className="absolute inset-0 bg-black/70 group-hover:bg-black/40 transition duration-500"></div>
 
-          <div className="relative z-10 flex flex-col justify-between h-full">
-            <h2 className="text-white font-extrabold text-2xl mb-3 flex items-end fontClass">
+          <div className="relative z-10 flex flex-col justify-between h-full pt-5 pr-10 pl-10 pb-3">
+            <h2 className="text-white font-extrabold text-4xl mb-3 flex items-end fontClass">
               UI/UX DESIGN{" "}
               <div className="w-full h-[1px] bg-white/40 my-2"></div>
-            </h2>
-            <p className="text-gray-300 text-sm leading-relaxed flex-grow">
-              Elevate your digital frontier with AI-crafted interfaces that
-              captivate and convert. Our designs don't just look stunning—they
-              ignite user obsession with seamless, futuristic flow. Over 150
-              projects completed, transforming 120+ brands.
-            </p>
-            <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
-              CHECK OUT WORK
-            </button>
+            </h2>{" "}
+            <div className="flex flex-col justify-between">
+              <p className="text-gray-300 text-sm leading-relaxed flex-grow">
+                Elevate your digital frontier with AI-crafted interfaces that
+                captivate and convert. Our designs don’t just look stunning they
+                ignite user obsession with seamless, futuristic flow. Over 150
+                projects completed, transforming 120+ brands.
+              </p>
+              <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
+                CHECK OUT WORK
+              </button>
+            </div>
           </div>
         </div>
-        <div className="relative rounded-2xl overflow-hidden group bg-neutral-600 p-6 flex flex-col justify-between w-[60%] mb-6">
+        <div className="relative rounded-4xl overflow-hidden group bg-neutral-500 p-6 flex flex-col justify-between w-[60%] mb-6">
           <video
             src="/IMG_3422.mp4"
             autoPlay
@@ -45,22 +47,25 @@ export default function Services() {
           />
           <div className="absolute inset-0 bg-black/70 group-hover:bg-black/40 transition duration-500"></div>
 
-          <div className="relative z-10 flex flex-col justify-between h-full">
-            <h2 className="text-white font-extrabold text-2xl mb-3 flex items-end fontClass">
+          <div className="relative z-10 flex flex-col justify-between h-full pt-5 pr-10 pl-10 pb-3">
+            <h2 className="text-white font-extrabold text-4xl mb-3 flex items-end fontClass">
               VIDEO EDITING
               <div className="w-full h-[1px] bg-white/40 my-2 m-2 "></div>
             </h2>
-            <p className="text-gray-300 text-sm leading-relaxed flex-grow">
-              Unleash your brand’s cosmic potential. We fuse AI precision with
-              bold creativity, forging identities and strategies that dominate
-              markets. Proudly delivered 200+ campaigns for 85 clients.
-            </p>
-            <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
-              CHECK OUT WORK
-            </button>
+            <div className="flex flex-col justify-between">
+              <p className="text-gray-300 text-sm leading-relaxed flex-grow">
+                Unleash your brand’s cosmic potential. We fuse AI precision with
+                bold creativity, forging identities and strategies that dominate
+                markets and soar beyond the competition. Proudly delivered 200+
+                campaigns for 85 clients.
+              </p>
+              <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
+                CHECK OUT WORK
+              </button>
+            </div>
           </div>
         </div>
-        <div className="relative rounded-2xl overflow-hidden group bg-neutral-600 p-6 flex flex-col justify-between w-[60%] mb-6">
+        <div className="relative rounded-4xl overflow-hidden group bg-neutral-500 p-6 flex flex-col justify-between w-[70%] mb-6">
           <video
             src="/IMG_3423.mp4"
             autoPlay
@@ -71,23 +76,26 @@ export default function Services() {
           />
           <div className="absolute inset-0 bg-black/70 group-hover:bg-black/40 transition duration-500"></div>
 
-          <div className="relative z-10 flex flex-col justify-between h-full">
-            <h2 className="text-white font-extrabold text-2xl mb-3 flex items-end fontClass">
+          <div className="relative z-10 flex flex-col justify-between h-full pt-5 pr-10 pl-10 pb-3">
+            <h2 className="text-white font-extrabold text-4xl mb-3 flex items-end  fontClass">
               BRANDING MARKETING
               <div className="w-full h-[1px] bg-white/40 my-2 m-2 "></div>
             </h2>
-            <p className="text-gray-300 text-sm leading-relaxed flex-grow">
-              We craft bold, market-dominating strategies to build unforgettable
-              brand identities. From campaigns to execution, we ensure impact
-              and recognition.
-            </p>
-            <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
-              CHECK OUT WORK
-            </button>
+            <div className="flex flex-col justify-between">
+              <p className="text-gray-300 text-sm leading-relaxed flex-grow">
+                Unleash your brand’s cosmic potential. We fuse AI precision with
+                bold creativity, forging identities and strategies that dominate
+                markets and soar beyond the competition. Proudly delivered 200+
+                campaigns for 85 clients.
+              </p>
+              <button className="mt-6 border border-gray-400 px-4 py-2 rounded-full text-sm text-white hover:bg-white hover:text-black transition">
+                CHECK OUT WORK
+              </button>
+            </div>
           </div>
         </div>
-        <div className="aspect-square w-[400px] flex items-center justify-center">
-          <button className="bg-yellow-400 text-black font-bold rounded-full text-2xl hover:scale-110 transition flex items-center justify-center w-3/5 h-3/5 aspect-square fontClass">
+        <div className="w-[28%] flex items-center justify-center">
+          <button className="bg-[#FED573] text-black font-bold rounded-full text-4xl hover:scale-110 transition flex items-center justify-center w-full aspect-square ml-3 fontClass">
             HIRE US!
           </button>
         </div>
