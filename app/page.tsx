d427a17f0344c62a6fab6d62bfd42f987d523264
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2025-08-25 18:33:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Footer from "@/components/footer";
 import HeroSection from "@/components/home-section";
 import ScrollCard from "@/components/scroll-card";
+import WebsiteDesign from "@/components/website-section";
 
 export default function Home() {
   return (
     <>
       {/* <HeroSection /> */}
       <ScrollCard />
+      <WebsiteDesign />
+
+      <Footer />
     </>
   );
 }
