494fc752ff89e272a72bfa947ab17cd625d2ee0b
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2025-08-25 18:46:38 +0500

    Create header.tsx

---
+++ components/header.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import Link from "next/dist/client/link";
+import { Edit2 } from "lucide-react";
+
+const Header: React.FC = () => {
+  return (
+    <header
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "3rem",
+      }}
+    >
+      <img src="/logoBlack.png" alt="Logo" style={{ height: "60px" }} />
+      <Link href="#" title="Lets Talk" className="header__content__cta__button">
+        <span>Lets Talk</span>
+        <div className="header__content__cta__button__icon">
+          <Edit2 size={20} color="currentColor" />
+        </div>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
