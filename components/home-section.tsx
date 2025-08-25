77435949b4ae4033f5626bf23491cea36028b0f3
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2025-08-25 19:02:55 +0500

    hero update

--- components/home-section.tsx
+++ components/home-section.tsx
@@ -1,6 +1,29 @@
 "use client";
 import React, { useState, useEffect } from "react";
 
+const logos = [
+  "/herologo1.png",
+  "/herologo2.png",
+  "/herologo3.png",
+  "/herologo4.png",
+  "/herologo1.png",
+  "/herologo2.png",
+  "/herologo3.png",
+  "/herologo4.png",
+  "/herologo1.png",
+  "/herologo2.png",
+  "/herologo3.png",
+  "/herologo4.png",
+  "/herologo1.png",
+  "/herologo2.png",
+  "/herologo3.png",
+  "/herologo4.png",
+  "/herologo1.png",
+  "/herologo2.png",
+  "/herologo3.png",
+  "/herologo4.png",
+];
+
 const HeroSection: React.FC = () => {
   const [imageSrc, setImageSrc] = useState("/hero.gif");
 
@@ -16,6 +39,7 @@ const HeroSection: React.FC = () => {
     <section
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         minHeight: "70vh",
@@ -27,6 +51,41 @@ const HeroSection: React.FC = () => {
         alt="Hero GIF"
         style={{ maxWidth: "100%", height: "auto" }}
       />
+      <div
+        style={{
+          width: "100%",
+          overflow: "hidden",
+          marginTop: "2rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            gap: "2rem",
+            justifyContent: "center",
+            alignItems: "center",
+            animation: "scrollLogos 20s linear infinite",
+            minWidth: "max-content",
+          }}
+        >
+          {logos.map((logo, idx) => (
+            <img
+              key={idx}
+              src={logo}
+              alt={`Logo ${idx + 1}`}
+              style={{ height: "50px", width: "auto" }}
+            />
+          ))}
+        </div>
+        <style>
+          {`
+        @keyframes scrollLogos {
+          0% { transform: translateX(0); }
+          100% { transform: translateX(-50%); }
+        }
+          `}
+        </style>
+      </div>
     </section>
   );
 };
